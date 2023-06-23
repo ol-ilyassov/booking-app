ab95bd28dcbab34efacb1dba1b987c365dd5cbb6
Olzhas <olzhas.ilyassov@alg-in.kz>
2023-06-23 15:32:47 +0600

    update one point in tests

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -6,6 +6,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"time"
 
@@ -22,11 +23,19 @@ import (
 var app config.AppConfig
 var pathToTemplates = "./../../templates"
 var functions template.FuncMap
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 func getRoutes() http.Handler {
 	gob.Register(models.Reservation{})
 
 	app.InProduction = false
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
 
 	session := scs.New()
 	session.Lifetime = 24 * time.Hour
--- internal/render/setup_test.go
+++ internal/render/setup_test.go
@@ -2,6 +2,7 @@ package render
 
 import (
 	"encoding/gob"
+	"log"
 	"net/http"
 	"os"
 	"testing"
@@ -14,12 +15,20 @@ import (
 
 var session *scs.SessionManager
 var testApp config.AppConfig
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 func TestMain(m *testing.M) {
 	gob.Register(models.Reservation{})
 
 	// Application working mode (development|production).
 	testApp.InProduction = false
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	testApp.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	testApp.ErrorLog = errorLog
 
 	// Session management.
 	session = scs.New()
