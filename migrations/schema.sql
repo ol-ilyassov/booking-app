f7bbc633a6a05c98d7ce2b65c625fd6fb1329305
Olzhas <olzhas.ilyassov@alg-in.kz>
2023-06-24 17:41:54 +0600

    add db migrations

---
+++ migrations/schema.sql
@@ -0,0 +1,387 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.8 (Ubuntu 14.8-1.pgdg22.04+1)
+-- Dumped by pg_dump version 14.8 (Ubuntu 14.8-1.pgdg22.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: reservations; Type: TABLE; Schema: public; Owner: booker
+--
+
+CREATE TABLE public.reservations (
+    id integer NOT NULL,
+    first_name character varying(255) DEFAULT ''::character varying NOT NULL,
+    last_name character varying(255) DEFAULT ''::character varying NOT NULL,
+    email character varying(255) NOT NULL,
+    phone character varying(255) DEFAULT ''::character varying NOT NULL,
+    start_date date NOT NULL,
+    end_date date NOT NULL,
+    room_id integer NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.reservations OWNER TO booker;
+
+--
+-- Name: reservations_id_seq; Type: SEQUENCE; Schema: public; Owner: booker
+--
+
+CREATE SEQUENCE public.reservations_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.reservations_id_seq OWNER TO booker;
+
+--
+-- Name: reservations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: booker
+--
+
+ALTER SEQUENCE public.reservations_id_seq OWNED BY public.reservations.id;
+
+
+--
+-- Name: restrictions; Type: TABLE; Schema: public; Owner: booker
+--
+
+CREATE TABLE public.restrictions (
+    id integer NOT NULL,
+    restriction_name character varying(255) DEFAULT ''::character varying NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.restrictions OWNER TO booker;
+
+--
+-- Name: restrictions_id_seq; Type: SEQUENCE; Schema: public; Owner: booker
+--
+
+CREATE SEQUENCE public.restrictions_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.restrictions_id_seq OWNER TO booker;
+
+--
+-- Name: restrictions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: booker
+--
+
+ALTER SEQUENCE public.restrictions_id_seq OWNED BY public.restrictions.id;
+
+
+--
+-- Name: room_restrictions; Type: TABLE; Schema: public; Owner: booker
+--
+
+CREATE TABLE public.room_restrictions (
+    id integer NOT NULL,
+    start_date date NOT NULL,
+    end_date date NOT NULL,
+    room_id integer NOT NULL,
+    reservation_id integer NOT NULL,
+    restriction_id integer NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.room_restrictions OWNER TO booker;
+
+--
+-- Name: room_restrictions_id_seq; Type: SEQUENCE; Schema: public; Owner: booker
+--
+
+CREATE SEQUENCE public.room_restrictions_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.room_restrictions_id_seq OWNER TO booker;
+
+--
+-- Name: room_restrictions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: booker
+--
+
+ALTER SEQUENCE public.room_restrictions_id_seq OWNED BY public.room_restrictions.id;
+
+
+--
+-- Name: rooms; Type: TABLE; Schema: public; Owner: booker
+--
+
+CREATE TABLE public.rooms (
+    id integer NOT NULL,
+    room_name character varying(255) DEFAULT ''::character varying NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.rooms OWNER TO booker;
+
+--
+-- Name: rooms_id_seq; Type: SEQUENCE; Schema: public; Owner: booker
+--
+
+CREATE SEQUENCE public.rooms_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.rooms_id_seq OWNER TO booker;
+
+--
+-- Name: rooms_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: booker
+--
+
+ALTER SEQUENCE public.rooms_id_seq OWNED BY public.rooms.id;
+
+
+--
+-- Name: schema_migration; Type: TABLE; Schema: public; Owner: booker
+--
+
+CREATE TABLE public.schema_migration (
+    version character varying(14) NOT NULL
+);
+
+
+ALTER TABLE public.schema_migration OWNER TO booker;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: booker
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    first_name character varying(255) DEFAULT ''::character varying NOT NULL,
+    last_name character varying(255) DEFAULT ''::character varying NOT NULL,
+    email character varying(255) NOT NULL,
+    password character varying(60) NOT NULL,
+    access_level integer DEFAULT 1 NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO booker;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: booker
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_id_seq OWNER TO booker;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: booker
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: reservations id; Type: DEFAULT; Schema: public; Owner: booker
+--
+
+ALTER TABLE ONLY public.reservations ALTER COLUMN id SET DEFAULT nextval('public.reservations_id_seq'::regclass);
+
+
+--
+-- Name: restrictions id; Type: DEFAULT; Schema: public; Owner: booker
+--
+
+ALTER TABLE ONLY public.restrictions ALTER COLUMN id SET DEFAULT nextval('public.restrictions_id_seq'::regclass);
+
+
+--
+-- Name: room_restrictions id; Type: DEFAULT; Schema: public; Owner: booker
+--
+
+ALTER TABLE ONLY public.room_restrictions ALTER COLUMN id SET DEFAULT nextval('public.room_restrictions_id_seq'::regclass);
+
+
+--
+-- Name: rooms id; Type: DEFAULT; Schema: public; Owner: booker
+--
+
+ALTER TABLE ONLY public.rooms ALTER COLUMN id SET DEFAULT nextval('public.rooms_id_seq'::regclass);
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: booker
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Name: reservations reservations_pkey; Type: CONSTRAINT; Schema: public; Owner: booker
+--
+
+ALTER TABLE ONLY public.reservations
+    ADD CONSTRAINT reservations_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: restrictions restrictions_pkey; Type: CONSTRAINT; Schema: public; Owner: booker
+--
+
+ALTER TABLE ONLY public.restrictions
+    ADD CONSTRAINT restrictions_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: room_restrictions room_restrictions_pkey; Type: CONSTRAINT; Schema: public; Owner: booker
+--
+
+ALTER TABLE ONLY public.room_restrictions
+    ADD CONSTRAINT room_restrictions_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: rooms rooms_pkey; Type: CONSTRAINT; Schema: public; Owner: booker
+--
+
+ALTER TABLE ONLY public.rooms
+    ADD CONSTRAINT rooms_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: booker
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: idx_reservations_email; Type: INDEX; Schema: public; Owner: booker
+--
+
+CREATE INDEX idx_reservations_email ON public.reservations USING btree (email);
+
+
+--
+-- Name: idx_reservations_last_name; Type: INDEX; Schema: public; Owner: booker
+--
+
+CREATE INDEX idx_reservations_last_name ON public.reservations USING btree (last_name);
+
+
+--
+-- Name: idx_room_restrictions_reservation_id; Type: INDEX; Schema: public; Owner: booker
+--
+
+CREATE INDEX idx_room_restrictions_reservation_id ON public.room_restrictions USING btree (reservation_id);
+
+
+--
+-- Name: idx_room_restrictions_room_id; Type: INDEX; Schema: public; Owner: booker
+--
+
+CREATE INDEX idx_room_restrictions_room_id ON public.room_restrictions USING btree (room_id);
+
+
+--
+-- Name: idx_room_restrictions_start_date_end_date; Type: INDEX; Schema: public; Owner: booker
+--
+
+CREATE INDEX idx_room_restrictions_start_date_end_date ON public.room_restrictions USING btree (start_date, end_date);
+
+
+--
+-- Name: idx_users_email; Type: INDEX; Schema: public; Owner: booker
+--
+
+CREATE UNIQUE INDEX idx_users_email ON public.users USING btree (email);
+
+
+--
+-- Name: schema_migration_version_idx; Type: INDEX; Schema: public; Owner: booker
+--
+
+CREATE UNIQUE INDEX schema_migration_version_idx ON public.schema_migration USING btree (version);
+
+
+--
+-- Name: reservations fk_reservations_rooms; Type: FK CONSTRAINT; Schema: public; Owner: booker
+--
+
+ALTER TABLE ONLY public.reservations
+    ADD CONSTRAINT fk_reservations_rooms FOREIGN KEY (room_id) REFERENCES public.rooms(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: room_restrictions fk_room_restrictions_reservations; Type: FK CONSTRAINT; Schema: public; Owner: booker
+--
+
+ALTER TABLE ONLY public.room_restrictions
+    ADD CONSTRAINT fk_room_restrictions_reservations FOREIGN KEY (reservation_id) REFERENCES public.reservations(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: room_restrictions fk_room_restrictions_restrictions; Type: FK CONSTRAINT; Schema: public; Owner: booker
+--
+
+ALTER TABLE ONLY public.room_restrictions
+    ADD CONSTRAINT fk_room_restrictions_restrictions FOREIGN KEY (restriction_id) REFERENCES public.restrictions(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: room_restrictions fk_room_restrictions_rooms; Type: FK CONSTRAINT; Schema: public; Owner: booker
+--
+
+ALTER TABLE ONLY public.room_restrictions
+    ADD CONSTRAINT fk_room_restrictions_rooms FOREIGN KEY (room_id) REFERENCES public.rooms(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
