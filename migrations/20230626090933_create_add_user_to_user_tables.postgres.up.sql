1c0c86821a7552531557e2861bc32df0c0f55dc4
Olzhas <olzhas.ilyassov@alg-in.kz>
2023-06-26 16:19:30 +0600

    add admin data

---
+++ migrations/20230626090933_create_add_user_to_user_tables.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (first_name,last_name,email,"password",access_level,created_at,updated_at) VALUES
+	 ('Admin','Admin','admin@admin.com','$2a$12$7kXyvPSWnEzWKSGHtaZlxuyPvPQiZt3zW3.1ibBVfQUv5BGHaC0gu',3,'2023-01-01 00:00:00','2023-01-01 00:00:00');
